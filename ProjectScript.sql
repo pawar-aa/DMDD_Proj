cd7df46b5465c1cca8d3d168ec8dfac9ab077771
Aashay Pawar <pawar.aa@northeastern.edu>
2022-11-20 17:24:21 -0500

    Aashay_commit_2

--- ProjectScript.sql
+++ ProjectScript.sql
@@ -17,3 +17,26 @@ select 4,'Nency','Panchani',4567890123,1,380,40 from dual
 where not exists(select * from user_table where user_id = 4);
 
 
+-- Insert ADDRESS
+
+insert into address(address_id,address,street,city,state,pin,country,user_id)
+select 101,'Longwood Apt','Tremont','Boston','MA',2120,'USA',1 from dual
+where not exists(select * from address where address_id = 101);
+
+insert into address(address_id,address,street,city,state,pin,country,user_id)
+select 102,'JVUE Apt','Park Street','Chicago','Illinois',1324,'USA',2 from dual
+where not exists(select * from address where address_id = 102);
+
+insert into address(address_id,address,street,city,state,pin,country,user_id)
+select 103,'Madisson Part Ct','Shawmut','Boston','MA',2115,'USA',3 from dual
+where not exists(select * from address where address_id = 103);
+
+insert into address(address_id,address,street,city,state,pin,country,user_id)
+select 104,'Brookline','Kenmore','Boston','MA',2112,'USA',4 from dual
+where not exists(select * from address where address_id = 104);
+
+
+
+
+
+
